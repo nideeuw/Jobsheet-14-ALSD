15ecc7f96c489b7f6c3bb6c958be043b28f62c5e
LAPTOP-RBLOQQKD\Naswa Nida <naswanida3@gmail.com>
2025-06-04 14:48:48 +0700

    Percobaan 1

---
+++ Node17.java
@@ -0,0 +1,13 @@
+public class Node17 {
+    Mahasiswa17 mahasiswa;
+    Node17 left, right;
+
+    public Node17(){
+        
+    }
+    public Node17(Mahasiswa17 mahasiswa){
+        this.mahasiswa = mahasiswa;
+        this.left = null;
+        left = right = null;
+    }
+}
