80ee4303b0cca13072291e18338177df3dd3fd21
LAPTOP-RBLOQQKD\Naswa Nida <naswanida3@gmail.com>
2025-06-08 22:37:51 +0700

    Tugas 4

--- BinaryTreeArray17.java
+++ BinaryTreeArray17.java
@@ -4,10 +4,19 @@ public class BinaryTreeArray17 {
 
     public BinaryTreeArray17() {
         this.dataMahasiswa = new Mahasiswa17[10];
+        this.idxLast = -1;
     }
     void populateData(Mahasiswa17 dataMhs[], int idxLast) {
         this.dataMahasiswa = dataMhs;
         this.idxLast = idxLast;
+    }
+    public void add(Mahasiswa17 mhs) {
+        if (idxLast < dataMahasiswa.length - 1) {
+            idxLast++;
+            dataMahasiswa[idxLast] = mhs;
+        } else {
+            System.out.println("Tree penuh");
+        }
     }
     void traverseInOrder(int idxStart) {
         if (idxStart <= idxLast) {
@@ -16,6 +25,13 @@ public class BinaryTreeArray17 {
                 dataMahasiswa[idxStart].tampilInformasi();
                 traverseInOrder(2 * idxStart + 2);
             }
+        }
+    }
+    void traversePreOrder(int idxStart) {
+        if (idxStart <= idxLast && dataMahasiswa[idxStart] != null) {
+            dataMahasiswa[idxStart].tampilInformasi();
+            traversePreOrder(2 * idxStart + 1);
+            traversePreOrder(2 * idxStart + 2);
         }
     }
 }
